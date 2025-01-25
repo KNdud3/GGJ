b9e6dcd2b5e7526bd4c430fac6c0831592242479
KNdud3 <krishansachdev@hotmail.co.uk>
2025-01-25 15:24:42 +0000

    feat(General): added comments

--- Assets/Scripts/Textbubble.cs
+++ Assets/Scripts/Textbubble.cs
@@ -2,26 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class scr : MonoBehaviour
 {
     private string[] dialogue = { "This is first", "this is last" };
-    private int index = 0;
+    private int dialogueIndex = 0;
     public TextMeshProUGUI bubbleText;
+
+    public float textSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
-        bubbleText.text = "Hello, TMP World!";
+        bubbleText.text = "";
+        StartCoroutine(SlowPrint());
     }
 
     // Update is called once per frame
     void Update()
     {
-        bubbleText.text = dialogue[index];
         if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return))
         {
-            index++;
+            if (bubbleText.text == dialogue[dialogueIndex])
+            {
+                bubbleText.text = "";
+                dialogueIndex++;
+                if (dialogueIndex >= dialogue.Length)
+                {
+                    SceneManager.LoadScene(0); // I hard coded this don't mess with the build scene order
+                }
+                else
+                {
+                    StartCoroutine(SlowPrint());
+                }
+            }
+            else
+            {
+                StopAllCoroutines();
+                bubbleText.text = dialogue[dialogueIndex];
+            }
+        }
+    }
+    // Prints the characters one by one
+    IEnumerator SlowPrint()
+    {
+        foreach (char c in dialogue[dialogueIndex])
+        {
+            bubbleText.text += c;
+            yield return new WaitForSeconds(textSpeed);
         }
     }
 }
