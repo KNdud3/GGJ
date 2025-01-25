b22c0be8d521d18e125062df81505f2aabbe4e38
Mark <email>
2025-01-25 16:36:51 +0000

    two circles that can be pressed

---
+++ Assets/Scripts/CirclePress.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleClickHandler : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log(gameObject.name + " clicky");
+    }
+}
