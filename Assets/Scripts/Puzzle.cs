a4eb91e4a1e3f61cfe3cd643c95e24a7cf2c18b4
Pran-bho <prantanilbhowmick@gmail.com>
2025-01-26 14:42:27 +0000

    cosfn
    conflic

--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -4,15 +4,11 @@ using System.Collections.Generic;
 public class Puzzle : MonoBehaviour
 {
     private List<Node> nodes = new List<Node>();
+    public int puzzleIndex;
     
-    public void Start()
+    private void Start()
     {
-        nodes.Clear();
-        nodes.AddRange(GetComponentsInChildren<Node>(true));
-        foreach(var node in nodes)
-        {
-            node.gameObject.SetActive(true);
-        }
+        nodes.AddRange(GetComponentsInChildren<Node>());
     }
 
     private void Update()
